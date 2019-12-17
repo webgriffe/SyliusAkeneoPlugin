18d91f9d3346462c23215f6ef96d60785463094b
Luca Gallinari <luke.gallinari@gmail.com>
2019-12-17 18:08:50 +0100

    Defines import queue item entity

---
+++ src/Entity/ImportQueueItem.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusAkeneoPlugin\Entity;
+
+final class ImportQueueItem implements ImportQueueItemInterface
+{
+    /**
+     * @var string
+     */
+    private $identifier;
+
+    /**
+     * @var string
+     */
+    private $entity;
+    /**
+     * @var \DateTimeInterface
+     */
+    private $createdAt;
+    /**
+     * @var \DateTimeInterface|null
+     */
+    private $importedAt;
+
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getEntity(): string
+    {
+        return $this->entity;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function getImportedAt(): ?\DateTimeInterface
+    {
+        return $this->importedAt;
+    }
+
+    /**
+     * @param string $identifier
+     */
+    public function setIdentifier(string $identifier): void
+    {
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * @param string $entity
+     */
+    public function setEntity(string $entity): void
+    {
+        $this->entity = $entity;
+    }
+
+    /**
+     * @param \DateTimeInterface $createdAt
+     */
+    public function setCreatedAt(\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $importedAt
+     */
+    public function setImportedAt(?\DateTimeInterface $importedAt): void
+    {
+        $this->importedAt = $importedAt;
+    }
+}
---
+++ src/Entity/ImportQueueItemInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusAkeneoPlugin\Entity;
+
+interface ImportQueueItemInterface
+{
+    public function getIdentifier(): string;
+
+    public function getEntity(): string;
+
+    public function getCreatedAt(): \DateTimeInterface;
+
+    public function getImportedAt(): ?\DateTimeInterface;
+
+    public function setIdentifier(string $identifier): void;
+
+    public function setEntity(string $entity): void;
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): void;
+
+    public function setImportedAt(?\DateTimeInterface $importedAt): void;
+}
