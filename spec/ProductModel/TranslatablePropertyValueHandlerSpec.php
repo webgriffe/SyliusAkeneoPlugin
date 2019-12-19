24501e71e0b9fc5dfba205776b7b53846c783232
Manuele Menozzi <mmenozzi@webgriffe.com>
2019-12-19 11:35:06 +0100

    Turn NameValueHandler into TranslatablePropertyValueHanlder

---
+++ spec/ProductModel/TranslatablePropertyValueHandlerSpec.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Webgriffe\SyliusAkeneoPlugin\ProductModel;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Component\Core\Model\Product;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductTranslation;
+use Sylius\Component\Core\Model\ProductTranslationInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Webgriffe\SyliusAkeneoPlugin\ProductModel\TranslatablePropertyValueHandler;
+use Webgriffe\SyliusAkeneoPlugin\ProductModel\ValueHandlerInterface;
+
+class TranslatablePropertyValueHandlerSpec extends ObjectBehavior
+{
+    private const AKENEO_ATTRIBUTE_CODE = 'akeneo_attribute_code';
+    private const TRANSLATION_PROPERTY_PATH = 'translation_property_path';
+
+    function let(FactoryInterface $productTranslationFactory, PropertyAccessorInterface $propertyAccessor)
+    {
+        $this->beConstructedWith($productTranslationFactory, $propertyAccessor, self::AKENEO_ATTRIBUTE_CODE, self::TRANSLATION_PROPERTY_PATH);
+    }
+
+    function it_implements_value_handler_interface()
+    {
+        $this->shouldHaveType(ValueHandlerInterface::class);
+    }
+
+    function it_supports_provided_akeneo_attribute_code()
+    {
+        $this->supports(new Product(), self::AKENEO_ATTRIBUTE_CODE, [])->shouldReturn(true);
+    }
+
+    function it_does_not_support_any_other_attribute_except_provided_akeneo_attribute_code()
+    {
+        $this->supports(new Product(), 'another_attribute', [])->shouldReturn(false);
+    }
+
+    function it_sets_value_on_an_already_existent_product_translation(
+        FactoryInterface $productTranslationFactory,
+        ProductTranslationInterface $existingProductTranslation,
+        ProductTranslationInterface $newProductTranslation,
+        PropertyAccessorInterface $propertyAccessor
+    ) {
+        $productTranslationFactory->createNew()->willReturn($newProductTranslation);
+        $product = new Product();
+        $product->addTranslation($existingProductTranslation->getWrappedObject());
+
+        $this->handle($product, 'name', [['locale' => 'en_US', 'scope' => null, 'data' => 'New name']]);
+
+        $propertyAccessor->setValue($existingProductTranslation, self::TRANSLATION_PROPERTY_PATH, 'New name')->shouldHaveBeenCalled();
+    }
+}
---
+++ src/ProductModel/TranslatablePropertyValueHandler.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusAkeneoPlugin\ProductModel;
+
+
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductTranslationInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+final class TranslatablePropertyValueHandler implements ValueHandlerInterface
+{
+    /**
+     * @var FactoryInterface
+     */
+    private $productTranslationFactory;
+    /**
+     * @var PropertyAccessorInterface
+     */
+    private $propertyAccessor;
+    /**
+     * @var string
+     */
+    private $akeneoAttributeCode;
+    /**
+     * @var string
+     */
+    private $translationPropertyPath;
+
+    public function __construct(
+        FactoryInterface $productTranslationFactory,
+        PropertyAccessorInterface $propertyAccessor,
+        string $akeneoAttributeCode,
+        string $translationPropertyPath
+    ) {
+        $this->productTranslationFactory = $productTranslationFactory;
+        $this->propertyAccessor = $propertyAccessor;
+        $this->akeneoAttributeCode = $akeneoAttributeCode;
+        $this->translationPropertyPath = $translationPropertyPath;
+    }
+
+    public function supports(ProductInterface $product, string $attribute, array $value): bool
+    {
+        return $attribute === $this->akeneoAttributeCode;
+    }
+
+    public function handle(ProductInterface $product, string $attribute, array $value)
+    {
+        foreach ($value as $item) {
+            /** @var ProductTranslationInterface $newTranslation */
+            $newTranslation = $this->productTranslationFactory->createNew();
+            $newTranslation->setLocale($item['locale']);
+            if (!$product->hasTranslation($newTranslation)) {
+                $product->addTranslation($newTranslation);
+            }
+            $translation = $product->getTranslation($item['locale']);
+            $this->propertyAccessor->setValue(
+                $translation,
+                $this->translationPropertyPath,
+                $item['data']
+            );
+        }
+    }
+}
