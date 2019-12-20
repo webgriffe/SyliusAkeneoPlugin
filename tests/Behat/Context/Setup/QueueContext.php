c1e1a677c9a2f27df36a1cce905e819f063fd585
Manuele Menozzi <mmenozzi@webgriffe.com>
2019-12-20 09:02:37 +0100

    Implement first version of behat QueueContext

--- tests/Behat/Context/Setup/QueueContext.php
+++ tests/Behat/Context/Setup/QueueContext.php
@@ -6,14 +6,37 @@ namespace Tests\Webgriffe\SyliusAkeneoPlugin\Behat\Context\Setup;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Tester\Exception\PendingException;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Webgriffe\SyliusAkeneoPlugin\Entity\QueueItemInterface;
+use Webgriffe\SyliusAkeneoPlugin\Repository\QueueItemRepositoryInterface;
 
 final class QueueContext implements Context
 {
+    /**
+     * @var FactoryInterface
+     */
+    private $queueItemFactory;
+    /**
+     * @var QueueItemRepositoryInterface
+     */
+    private $queueItemRepository;
+
+    public function __construct(FactoryInterface $queueItemFactory, QueueItemRepositoryInterface $queueItemRepository)
+    {
+        $this->queueItemFactory = $queueItemFactory;
+        $this->queueItemRepository = $queueItemRepository;
+    }
+
     /**
      * @Given /^there is one product model to import with identifier "([^"]*)" in the Akeneo queue$/
      */
     public function thereIsOneProductModelToImportWithIdentifierInTheAkeneoQueue(string $identifier)
     {
-        throw new PendingException();
+        /** @var QueueItemInterface $queueItem */
+        $queueItem = $this->queueItemFactory->createNew();
+        $queueItem->setAkeneoEntity(QueueItemInterface::AKENEO_ENTITY_PRODUCT_MODEL);
+        $queueItem->setAkeneoIdentifier($identifier);
+        $queueItem->setCreatedAt(new \DateTime());
+        $this->queueItemRepository->add($queueItem);
     }
 }
