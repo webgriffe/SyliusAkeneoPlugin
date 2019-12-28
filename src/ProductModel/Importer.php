a7f3fc70d5d35c818b73bc88a89adda4420a4ea5
Manuele Menozzi <manuele.menozzi@gmail.com>
2019-12-28 18:16:17 +0100

    Add pre/post create/update events dispatch to ProductModel\Importer

--- src/ProductModel/Importer.php
+++ src/ProductModel/Importer.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusAkeneoPlugin\ProductModel;
 
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\Component\Product\Factory\ProductFactoryInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Webgriffe\SyliusAkeneoPlugin\ApiClientInterface;
 use Webgriffe\SyliusAkeneoPlugin\ImporterInterface;
 use Webmozart\Assert\Assert;
@@ -30,6 +33,10 @@ final class Importer implements ImporterInterface
 
     /** @var ApiClientInterface */
     private $apiClient;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
 
     public function __construct(
         ProductRepositoryInterface $productRepository,
@@ -37,7 +44,8 @@ final class Importer implements ImporterInterface
         CategoriesHandlerInterface $categoriesHandler,
         FamilyVariantHandlerInterface $familyVariantHandler,
         ValueHandlerResolverInterface $valueHandlerResolver,
-        ApiClientInterface $apiClient
+        ApiClientInterface $apiClient,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->productRepository = $productRepository;
         $this->productFactory = $productFactory;
@@ -45,6 +53,7 @@ final class Importer implements ImporterInterface
         $this->familyVariantHandler = $familyVariantHandler;
         $this->valueHandlerResolver = $valueHandlerResolver;
         $this->apiClient = $apiClient;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function import(string $identifier): void
@@ -53,7 +62,9 @@ final class Importer implements ImporterInterface
         $productModelResponse = $this->apiClient->findProductModel($identifier);
         $code = $productModelResponse['code'];
         $product = $this->productRepository->findOneByCode($code);
+        $eventName = 'update';
         if (!$product) {
+            $eventName = 'create';
             $product = $this->productFactory->createNew();
         }
         Assert::isInstanceOf($product, ProductInterface::class);
@@ -77,8 +88,24 @@ final class Importer implements ImporterInterface
 
         $this->familyVariantHandler->handle($product, $familyVariantResponse);
 
+        $this->dispatchPreEvent($product, $eventName);
+        // TODO We should handle $event->isStopped() where $event is the return value of the dispatchPreEvent method.
+        //      See \Sylius\Bundle\ResourceBundle\Controller\ResourceController.
         $this->productRepository->add($product);
-        // TODO We should dispatch sylius.product.pre/post_create/update events. For example, the ImageValueHandler
-        //      assumes that the \Sylius\Bundle\CoreBundle\EventListener\ImagesUploadListener is triggered.
+        $this->dispatchPostEvent($product, $eventName);
+    }
+
+    private function dispatchPreEvent(ResourceInterface $product, string $eventName): ResourceControllerEvent
+    {
+        $event = new ResourceControllerEvent($product);
+        $this->eventDispatcher->dispatch(sprintf('sylius.product.pre_%s', $eventName), $event);
+        return $event;
+    }
+
+    private function dispatchPostEvent(ResourceInterface $product, string $eventName): ResourceControllerEvent
+    {
+        $event = new ResourceControllerEvent($product);
+        $this->eventDispatcher->dispatch(sprintf('sylius.product.post_%s', $eventName), $event);
+        return $event;
     }
 }
