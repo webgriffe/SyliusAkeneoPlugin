0443dcc6b56b8fabc4e6a314137fcee923010e9f
Manuele Menozzi <manuele.menozzi@gmail.com>
2019-12-30 16:49:34 +0100

    Add ProductModel\Importer test to check if product belongs to option

--- tests/Integration/ProductModel/ImporterTest.php
+++ tests/Integration/ProductModel/ImporterTest.php
@@ -81,4 +81,28 @@ final class ImporterTest extends KernelTestCase
         $this->assertCount(1, $products[0]->getImages());
         $this->assertEquals('main', $products[0]->getImages()[0]->getType());
     }
+
+    /**
+     * @test
+     */
+    public function it_imports_product_model_with_proper_family_variant()
+    {
+        $this->fixtureLoader->load(
+            [
+                __DIR__ . '/../DataFixtures/ORM/resources/en_US_locale.yaml',
+                __DIR__ . '/../DataFixtures/ORM/resources/it_IT_locale.yaml',
+                __DIR__ . '/../DataFixtures/ORM/resources/size_product_option.yaml'
+            ],
+            [],
+            [],
+            PurgeMode::createDeleteMode()
+        );
+        $this->importer->import('model-braided-hat');
+        /** @var ProductInterface[] $products */
+        $products = $this->productRepository->findAll();
+        $this->assertCount(1, $products);
+        $this->assertEquals('Braided hat ', $products[0]->getName());
+        $this->assertCount(1, $products[0]->getOptions());
+        $this->assertEquals('size', $products[0]->getOptions()[0]->getCode());
+    }
 }
