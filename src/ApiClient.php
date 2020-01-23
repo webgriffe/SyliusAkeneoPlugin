ccfcea8a59b98696e32d98698ff496b65e9b37dd
Luca Gallinari <luke.gallinari@gmail.com>
2020-01-23 12:37:41 +0100

    Implements find modified products after date on Api Client

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -4,8 +4,50 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusAkeneoPlugin;
 
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Request;
+use Webmozart\Assert\Assert;
+
 final class ApiClient implements ApiClientInterface
 {
+    /** @var string */
+    private $token;
+
+    /** @var ClientInterface */
+    private $httpClient;
+
+    /** @var string */
+    private $baseUrl;
+
+    /** @var string */
+    private $username;
+
+    /** @var string */
+    private $password;
+
+    /** @var string */
+    private $clientId;
+
+    /** @var string */
+    private $secret;
+
+    public function __construct(
+        ClientInterface $httpClient,
+        string $baseUrl,
+        string $username,
+        string $password,
+        string $clientId,
+        string $secret
+    ) {
+        $this->httpClient = $httpClient;
+        $this->baseUrl = rtrim($baseUrl, '/');
+        $this->username = $username;
+        $this->password = $password;
+        $this->clientId = $clientId;
+        $this->secret = $secret;
+    }
+
     public function findProductModel(string $code): ?array
     {
         // TODO: Implement findProductModelByIdentifier() method.
@@ -42,9 +84,82 @@ final class ApiClient implements ApiClientInterface
         return null;
     }
 
+    /**
+     * @throws GuzzleException
+     * @throws \HttpException
+     */
     public function findProductsModifiedAfter(\DateTime $date): ?array
     {
-        // TODO: Implement findProductsModifiedAfter() method.
-        return null;
+        if (!$this->token) {
+            $this->login();
+        }
+        $endpoint = sprintf(
+            '/api/rest/v1/products?search={"updated":[{"operator":">","value":"%s"}]}',
+            $date->format('Y-m-d H:i:s')
+        );
+
+        $responseResult = $this->doRequest($endpoint);
+
+        $items = $responseResult['_embedded']['items'];
+        $identifiers = [];
+        foreach ($items as $item) {
+            $identifiers[] = $item['identifier'];
+        }
+
+        return $identifiers;
+    }
+
+    private function login(): void
+    {
+        $body = json_encode(
+            [
+                'grant_type' => 'password',
+                'username' => $this->username,
+                'password' => $this->password,
+            ]
+        );
+        Assert::string($body);
+        $headers = [
+            'Content-Type' => 'application/json',
+        ];
+        $request = new Request(
+            'POST',
+            $this->baseUrl . '/api/oauth/v1/token',
+            $headers,
+            $body
+        );
+        $options = [
+            'auth' => [
+                $this->clientId,
+                $this->secret,
+            ],
+        ];
+        $rawResponse = $this->httpClient->send($request, $options);
+        $responseResult = json_decode($rawResponse->getBody()->getContents(), true);
+
+        $this->token = $responseResult['access_token'];
+    }
+
+    /**
+     * @return mixed
+     *
+     * @throws GuzzleException
+     * @throws \HttpException
+     */
+    private function doRequest(string $endpoint)
+    {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' => sprintf('Bearer %s', $this->token),
+        ];
+        $request = new Request('GET', $this->baseUrl . $endpoint, $headers);
+        $response = $this->httpClient->send($request);
+        $statusClass = (int) ($response->getStatusCode() / 100);
+        $responseResult = json_decode($response->getBody()->getContents(), true);
+        if ($statusClass !== 2) {
+            throw new \HttpException($responseResult['message'], $responseResult['code']);
+        }
+
+        return $responseResult;
     }
 }
