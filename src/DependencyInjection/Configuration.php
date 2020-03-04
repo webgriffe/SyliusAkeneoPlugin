cb073519e7fd6cd3196f994084bafd6394dc4c8d
Manuele Menozzi <manuele.menozzi@gmail.com>
2020-03-04 22:14:47 +0100

    Add friendly configuration for product import value handlers

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,34 +19,57 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('webgriffe_sylius_akeneo_plugin');
+        $treeBuilder = new TreeBuilder('webgriffe_sylius_akeneo');
         if (\method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->getRootNode();
         } else {
             // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('webgriffe_sylius_akeneo_plugin');
+            $rootNode = $treeBuilder->root('webgriffe_sylius_akeneo');
         }
 
         $rootNode
             ->children()
-                ->arrayNode('resources')
-                ->addDefaultsIfNotSet()
-                ->children()
-                    ->arrayNode('queue_item')
-                        ->addDefaultsIfNotSet()
-                        ->children()
-                            ->variableNode('options')->end()
-                            ->arrayNode('classes')
-                                ->addDefaultsIfNotSet()
+
+                ->arrayNode('value_handlers')
+                    ->children()
+                        ->arrayNode('product')
+                            ->arrayPrototype()
                                 ->children()
-                                    ->scalarNode('model')->defaultValue(QueueItem::class)->cannotBeEmpty()->end()
-                                    ->scalarNode('interface')->defaultValue(QueueItemInterface::class)->cannotBeEmpty()->end()
-                                    ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
-                                    ->scalarNode('factory')->defaultValue(Factory::class)->cannotBeEmpty()->end()
-                                    ->scalarNode('repository')->defaultValue(QueueItemRepository::class)->cannotBeEmpty()->end()
+                                    ->enumNode('type')
+                                        ->values(
+                                            array_keys(WebgriffeSyliusAkeneoExtension::$valueHandlersTypesDefinitions)
+                                        )
+                                        ->isRequired()
+                                    ->end()
+                                    ->arrayNode('options')
+                                        ->variablePrototype()
+                                        ->end()
+                                    ->end()
                                 ->end()
+                            ->end()
                         ->end()
+                    ->end()
                 ->end()
+
+                ->arrayNode('resources')->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('queue_item')->addDefaultsIfNotSet()
+                            ->children()
+                                ->variableNode('options')->end()
+                                ->arrayNode('classes')->addDefaultsIfNotSet()
+                                    ->children()
+                                        ->scalarNode('model')->defaultValue(QueueItem::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('interface')->defaultValue(QueueItemInterface::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('controller')->defaultValue(ResourceController::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('factory')->defaultValue(Factory::class)->cannotBeEmpty()->end()
+                                        ->scalarNode('repository')->defaultValue(QueueItemRepository::class)->cannotBeEmpty()->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+
             ->end()
         ;
 
--- src/DependencyInjection/WebgriffeSyliusAkeneoExtension.php
+++ src/DependencyInjection/WebgriffeSyliusAkeneoExtension.php
@@ -7,10 +7,72 @@ namespace Webgriffe\SyliusAkeneoPlugin\DependencyInjection;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Webgriffe\SyliusAkeneoPlugin\ValueHandler\ChannelPricingValueHandler;
+use Webgriffe\SyliusAkeneoPlugin\ValueHandler\GenericPropertyValueHandler;
+use Webgriffe\SyliusAkeneoPlugin\ValueHandler\ImageValueHandler;
+use Webgriffe\SyliusAkeneoPlugin\ValueHandler\ImmutableSlugValueHandler;
+use Webgriffe\SyliusAkeneoPlugin\ValueHandler\ProductOptionValueHandler;
+use Webgriffe\SyliusAkeneoPlugin\ValueHandler\TranslatablePropertyValueHandler;
 
 final class WebgriffeSyliusAkeneoExtension extends AbstractResourceExtension
 {
+    /** @var array */
+    public static $valueHandlersTypesDefinitions = [
+        'channel_pricing' => [
+            'class' => ChannelPricingValueHandler::class,
+            'arguments' => [
+                'sylius.factory.channel_pricing',
+                'sylius.repository.channel',
+                'sylius.repository.currency',
+            ],
+        ],
+        'generic_property' => [
+            'class' => GenericPropertyValueHandler::class,
+            'arguments' => [
+                'property_accessor',
+            ],
+        ],
+        'image' => [
+            'class' => ImageValueHandler::class,
+            'arguments' => [
+                'sylius.factory.product_image',
+                'sylius.repository.product_image',
+                'webgriffe_sylius_akeneo.api_client',
+            ],
+        ],
+        'immutable_slug' => [
+            'class' => ImmutableSlugValueHandler::class,
+            'arguments' => [
+                'sonata.core.slugify.cocur',
+                'sylius.factory.product_translation',
+                'sylius.translation_locale_provider.admin',
+                'sylius.repository.product_translation',
+            ],
+        ],
+        'product_option' => [
+            'class' => ProductOptionValueHandler::class,
+            'arguments' => [
+                'webgriffe_sylius_akeneo.api_client',
+                'sylius.repository.product_option',
+                'sylius.factory.product_option_value',
+                'sylius.factory.product_option_value_translation',
+                'sylius.repository.product_option_value',
+            ],
+        ],
+        'translatable_property' => [
+            'class' => TranslatablePropertyValueHandler::class,
+            'arguments' => [
+                'property_accessor',
+                'sylius.factory.product_translation',
+                'sylius.factory.product_variant_translation',
+                'sylius.translation_locale_provider.admin',
+            ],
+        ],
+    ];
+
     /**
      * {@inheritdoc}
      */
@@ -22,5 +84,41 @@ final class WebgriffeSyliusAkeneoExtension extends AbstractResourceExtension
         $this->registerResources('webgriffe_sylius_akeneo_plugin', 'doctrine/orm', $config['resources'], $container);
 
         $loader->load('services.xml');
+
+        $productValueHandlersDefinitions = $this->createValueHandlersDefinitions($config['value_handlers']['product']);
+        $container->addDefinitions($productValueHandlersDefinitions);
+
+        if ($container->hasDefinition('webgriffe_sylius_akeneo.product.value_handlers_resolver')) {
+            $resolverDefinition = $container->getDefinition('webgriffe_sylius_akeneo.product.value_handlers_resolver');
+            foreach (array_keys($productValueHandlersDefinitions) as $reference) {
+                $resolverDefinition->addMethodCall('add', [new Reference($reference)]);
+            }
+        }
+    }
+
+    /**
+     * @return Definition[]
+     */
+    private function createValueHandlersDefinitions(array $valueHandlers): array
+    {
+        $definitions = [];
+        foreach ($valueHandlers as $name => $valueHandler) {
+            $type = $valueHandler['type'];
+            $options = $valueHandler['options'] ?? [];
+
+            $arguments = array_merge(
+                array_map(
+                    function (string $argument) {
+                        return new Reference($argument);
+                    },
+                    self::$valueHandlersTypesDefinitions[$type]['arguments']
+                ),
+                array_values($options)
+            );
+            $id = sprintf('webgriffe_sylius_akeneo.value_handler.product.%s_value_handler', $name);
+            $definitions[$id] = new Definition(self::$valueHandlersTypesDefinitions[$type]['class'], $arguments);
+        }
+
+        return $definitions;
     }
 }
