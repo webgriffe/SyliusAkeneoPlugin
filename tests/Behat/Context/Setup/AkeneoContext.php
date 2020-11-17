f9f5184760f88ca4fdf897eab4a64c07f68d3d8d
Manuele Menozzi <manuele.menozzi@gmail.com>
2020-11-17 08:18:23 +0100

    Replace API client mock even in behat tests

--- tests/Behat/Context/Setup/AkeneoContext.php
+++ tests/Behat/Context/Setup/AkeneoContext.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Tests\Webgriffe\SyliusAkeneoPlugin\Behat\Context\Setup;
 
+use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
 use Behat\Behat\Context\Context;
-use Tests\Webgriffe\SyliusAkeneoPlugin\Integration\TestDouble\ApiClientMock;
-use Webgriffe\SyliusAkeneoPlugin\ApiClientInterface;
+use Tests\Webgriffe\SyliusAkeneoPlugin\Integration\TestDouble\OfficialApiClientMock;
 use Webmozart\Assert\Assert;
 
 final class AkeneoContext implements Context
 {
-    /** @var ApiClientInterface|ApiClientMock */
+    /** @var AkeneoPimClientInterface|OfficialApiClientMock */
     private $apiClient;
 
-    public function __construct(ApiClientInterface $apiClient)
+    public function __construct(AkeneoPimClientInterface $apiClient)
     {
         $this->apiClient = $apiClient;
     }
@@ -24,7 +24,7 @@ final class AkeneoContext implements Context
      */
     public function thereIsAProductUpdatedAtOnAkeneo(string $identifier, \DateTime $date)
     {
-        Assert::isInstanceOf($this->apiClient, ApiClientMock::class);
+        Assert::isInstanceOf($this->apiClient, OfficialApiClientMock::class);
         $this->apiClient->addProductUpdatedAt($identifier, $date);
     }
 
@@ -33,6 +33,7 @@ final class AkeneoContext implements Context
      */
     public function thereAreProductsOnAkeneo(int $count)
     {
+        Assert::isInstanceOf($this->apiClient, OfficialApiClientMock::class);
         for ($i = 1; $i <= $count; ++$i) {
             $this->apiClient->addProductUpdatedAt('product-' . $i, new \DateTime());
         }
