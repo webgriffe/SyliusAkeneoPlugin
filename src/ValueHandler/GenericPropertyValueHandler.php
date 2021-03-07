a114378bc2c6b263af961ec80b89d2186b78aea4
Manuele Menozzi <mmenozzi@webgriffe.com>
2021-03-07 18:28:43 +0100

    Make GenericPropertyValueHandler write product and variant

--- src/ValueHandler/GenericPropertyValueHandler.php
+++ src/ValueHandler/GenericPropertyValueHandler.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusAkeneoPlugin\ValueHandler;
 
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 use Webgriffe\SyliusAkeneoPlugin\ValueHandlerInterface;
+use Webmozart\Assert\Assert;
 
 final class GenericPropertyValueHandler implements ValueHandlerInterface
 {
@@ -33,7 +36,7 @@ final class GenericPropertyValueHandler implements ValueHandlerInterface
      */
     public function supports($subject, string $attribute, array $value): bool
     {
-        return $attribute === $this->akeneoAttributeCode;
+        return $subject instanceof ProductVariantInterface && $attribute === $this->akeneoAttributeCode;
     }
 
     /**
@@ -45,12 +48,37 @@ final class GenericPropertyValueHandler implements ValueHandlerInterface
             throw new \InvalidArgumentException(
                 sprintf(
                     'Cannot handle Akeneo attribute "%s". %s only supports Akeneo attribute "%s".',
-                    'not_supported_property',
+                    $attribute,
                     self::class,
                     $this->akeneoAttributeCode
                 )
             );
         }
-        $this->propertyAccessor->setValue($subject, $this->propertyPath, $value[0]['data']);
+        $hasBeenSet = false;
+
+        $productVariant = $subject;
+        Assert::isInstanceOf($productVariant, ProductVariantInterface::class);
+        if ($this->propertyAccessor->isWritable($productVariant, $this->propertyPath)) {
+            $this->propertyAccessor->setValue($productVariant, $this->propertyPath, $value[0]['data']);
+            $hasBeenSet = true;
+        }
+
+        $product = $productVariant->getProduct();
+        Assert::isInstanceOf($product, ProductInterface::class);
+        if ($this->propertyAccessor->isWritable($product, $this->propertyPath)) {
+            $this->propertyAccessor->setValue($product, $this->propertyPath, $value[0]['data']);
+            $hasBeenSet = true;
+        }
+
+        if (!$hasBeenSet) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Property path "%s" is not writable on both %s and %s but it should be for at least once.',
+                    $this->propertyPath,
+                    get_class($productVariant),
+                    get_class($product)
+                )
+            );
+        }
     }
 }
