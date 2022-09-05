f774d18f7c717105bbc77cc9350f4ae7d3ae6fc9
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-09-05 12:39:38 +0200

    Bump to PluginSkeleton master

--- ecs.php
+++ ecs.php
@@ -1,14 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
-    $parameters = $ecsConfig->parameters();
 
-    $parameters->set(Option::SKIP, [
+    $ecsConfig->parameters()->set(Option::SKIP, [
         VisibilityRequiredFixer::class => ['*Spec.php'],
     ]);
 };
