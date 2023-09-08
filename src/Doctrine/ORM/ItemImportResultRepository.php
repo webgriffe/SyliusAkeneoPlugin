f381b3a75be71a5f906c9bcd24cc5e45b00cad86
Manuele Menozzi <manuele.menozzi@gmail.com>
2023-09-08 17:27:22 +0200

    Implement ItemImportResultCleanupCommand (#168)

--- src/Doctrine/ORM/ItemImportResultRepository.php
+++ src/Doctrine/ORM/ItemImportResultRepository.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusAkeneoPlugin\Doctrine\ORM;
 
+use DateTimeInterface;
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Webgriffe\SyliusAkeneoPlugin\Entity\ItemImportResultInterface;
 use Webgriffe\SyliusAkeneoPlugin\Respository\ItemImportResultRepositoryInterface;
+use Webmozart\Assert\Assert;
 
 final class ItemImportResultRepository extends EntityRepository implements ItemImportResultRepositoryInterface
 {
+    public function findToCleanup(DateTimeInterface $dateLimit): array
+    {
+        $result = $this->createQueryBuilder('i')
+            ->where('i.createdAt IS NOT NULL')
+            ->andWhere('i.createdAt <= :dateLimit')
+            ->setParameter('dateLimit', $dateLimit)
+            ->getQuery()
+            ->getResult()
+        ;
+        Assert::isArray($result);
+
+        return $result;
+    }
 }
