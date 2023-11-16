1069c3121e9d7a3f8b0dc8eee2e9deb809909502
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-11-16 17:34:06 +0100

    Simple refactor to prepare next changes (#187)

--- src/AttributeOptions/Importer.php
+++ src/AttributeOptions/Importer.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusAkeneoPlugin\AttributeOptions;
 
 use Akeneo\Pim\ApiClient\AkeneoPimClientInterface;
+use Akeneo\Pim\ApiClient\Pagination\ResourceCursorInterface;
 use Akeneo\Pim\ApiClient\Search\SearchBuilder;
+use DateTime;
 use Sylius\Component\Attribute\AttributeType\SelectAttributeType;
 use Sylius\Component\Product\Model\ProductAttributeInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
@@ -13,6 +15,10 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Webgriffe\SyliusAkeneoPlugin\Event\IdentifiersModifiedSinceSearchBuilderBuiltEvent;
 use Webgriffe\SyliusAkeneoPlugin\ImporterInterface;
 
+/**
+ * @phpstan-type AkeneoAttribute array{code: string, type: string}
+ * @phpstan-type AkeneoAttributeOption array{_links: array, code: string, attribute: string, sort_order: int, labels: array<string, string>}
+ */
 final class Importer implements ImporterInterface
 {
     private const SIMPLESELECT_TYPE = 'pim_catalog_simpleselect';
@@ -37,24 +43,73 @@ final class Importer implements ImporterInterface
     public function import(string $identifier): void
     {
         $attribute = $this->attributeRepository->findOneBy(['code' => $identifier]);
-        if (null === $attribute) {
-            return;
+        if (null !== $attribute && $attribute->getType() === SelectAttributeType::TYPE) {
+            $this->importAttribute($identifier, $attribute);
+        }
+    }
+
+    /**
+     * As stated at https://api.akeneo.com/documentation/filter.html#by-update-date-3:
+     *
+     * > For Simple select and Multiple select attribute, an option update isn't considered as an attribute update.
+     *
+     * So, the $sinceDate argument it's not used here.
+     */
+    public function getIdentifiersModifiedSince(DateTime $sinceDate): array
+    {
+        $searchBuilder = new SearchBuilder();
+        $this->eventDispatcher->dispatch(
+            new IdentifiersModifiedSinceSearchBuilderBuiltEvent($this, $searchBuilder, $sinceDate),
+        );
+        /** @var ResourceCursorInterface<array-key, AkeneoAttribute> $akeneoAttributes */
+        $akeneoAttributes = $this->apiClient->getAttributeApi()->all(50, ['search' => $searchBuilder->getFilters()]);
+
+        return $this->filterBySyliusAttributeCodes($akeneoAttributes);
+    }
+
+    /**
+     * Return the list of Akeneo attribute codes whose code is used as a code for a Sylius attribute
+     *
+     * @param ResourceCursorInterface<array-key, AkeneoAttribute> $akeneoAttributes
+     *
+     * @return string[]
+     */
+    private function filterBySyliusAttributeCodes(ResourceCursorInterface $akeneoAttributes): array
+    {
+        $syliusSelectAttributes = $this->attributeRepository->findBy(['type' => SelectAttributeType::TYPE]);
+        $syliusSelectAttributes = array_filter(
+            array_map(
+                static fn (ProductAttributeInterface $attribute): ?string => $attribute->getCode(),
+                $syliusSelectAttributes,
+            ),
+        );
+        $attributeCodes = [];
+        foreach ($akeneoAttributes as $akeneoAttribute) {
+            if (!in_array($akeneoAttribute['code'], $syliusSelectAttributes, true)) {
+                continue;
+            }
+            if ($akeneoAttribute['type'] !== self::SIMPLESELECT_TYPE && $akeneoAttribute['type'] !== self::MULTISELECT_TYPE) {
+                continue;
+            }
+            $attributeCodes[] = $akeneoAttribute['code'];
         }
 
-        if ($attribute->getType() !== SelectAttributeType::TYPE) {
-            return;
-        }
+        return $attributeCodes;
+    }
 
+    private function importAttribute(string $attributeCode, ProductAttributeInterface $attribute): void
+    {
         $attributeOptionsOrdered = [];
-        $attributeOptions = $this->apiClient->getAttributeOptionApi()->all($identifier);
-        /** @var array $attributeOption */
+        /** @var ResourceCursorInterface<array-key, AkeneoAttributeOption> $attributeOptions */
+        $attributeOptions = $this->apiClient->getAttributeOptionApi()->all($attributeCode);
         foreach ($attributeOptions as $attributeOption) {
             $attributeOptionsOrdered[] = $attributeOption;
         }
         usort(
             $attributeOptionsOrdered,
-            static fn (array $option1, array $option2): int => ($option1['sort_order'] ?? 0) <=> ($option2['sort_order'] ?? 0),
+            static fn (array $option1, array $option2): int => $option1['sort_order'] <=> $option2['sort_order'],
         );
+        /** @var array{choices: array<string, array<string, string>>, multiple: bool, min: ?int, max: ?int} $configuration */
         $configuration = $attribute->getConfiguration();
         $configuration['choices'] = $this->convertAkeneoAttributeOptionsIntoSyliusChoices($attributeOptionsOrdered);
         $attribute->setConfiguration($configuration);
@@ -63,39 +118,10 @@ final class Importer implements ImporterInterface
     }
 
     /**
-     * As stated at https://api.akeneo.com/documentation/filter.html#by-update-date-3:
-     * > For Simple select and Multiple select attribute, an option update isn't considered as an attribute update.
-     * So, the $sinceDate argument it's not used here.
+     * @param array<array-key, AkeneoAttributeOption> $attributeOptions
+     *
+     * @return array<string, array<string, string>>
      */
-    public function getIdentifiersModifiedSince(\DateTime $sinceDate): array
-    {
-        $searchBuilder = new SearchBuilder();
-        $this->eventDispatcher->dispatch(
-            new IdentifiersModifiedSinceSearchBuilderBuiltEvent($this, $searchBuilder, $sinceDate),
-        );
-        /** @var array<array-key, array<string, mixed>> $akeneoAttributes */
-        $akeneoAttributes = $this->apiClient->getAttributeApi()->all(50, ['search' => $searchBuilder->getFilters()]);
-        $syliusSelectAttributes = $this->attributeRepository->findBy(['type' => SelectAttributeType::TYPE]);
-        $syliusSelectAttributes = array_filter(
-            array_map(
-                static fn (ProductAttributeInterface $attribute): ?string => $attribute->getCode(),
-                $syliusSelectAttributes,
-            ),
-        );
-        $identifiers = [];
-        foreach ($akeneoAttributes as $akeneoAttribute) {
-            if (!in_array($akeneoAttribute['code'], $syliusSelectAttributes, true)) {
-                continue;
-            }
-            if ($akeneoAttribute['type'] !== self::SIMPLESELECT_TYPE && $akeneoAttribute['type'] !== self::MULTISELECT_TYPE) {
-                continue;
-            }
-            $identifiers[] = $akeneoAttribute['code'];
-        }
-
-        return $identifiers;
-    }
-
     private function convertAkeneoAttributeOptionsIntoSyliusChoices(array $attributeOptions): array
     {
         $choices = [];
